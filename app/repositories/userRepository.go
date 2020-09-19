a6b139107ef90f84c66dd38734751b00cbac9e72
sirri_bakirtan <33KS333Dogan>
2020-09-19 10:25:56 +0300

    create user to database.

--- app/repositories/userRepository.go
+++ app/repositories/userRepository.go
@@ -1,10 +1,11 @@
 package repositories
 
 import (
-	"../models"
 	"context"
 	"errors"
 	"time"
+
+	"../models"
 )
 
 var UserRepository _UserRepository
@@ -24,8 +25,14 @@ func (_UserRepository) GetUser(user models.User) (models.User, error) {
 	return user, nil
 }
 
-func (_UserRepository) CreateUser(user models.User) bool {
-	return true
+func (_UserRepository) CreateUser(user models.User) (models.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	_, err := UserRepository.collection.InsertOne(ctx, user)
+	if err != nil {
+		return models.User{}, errors.New("Not Saved")
+	}
+	return user, nil
 }
 
 func (_UserRepository) UpdateUser(user models.User) bool {
--- app/services/userService.go
+++ app/services/userService.go
@@ -18,3 +18,10 @@ func (_UserService) Login(user models.User) bool {
 	}
 	return true
 }
+func (_UserService) SignUp(user models.User) bool {
+	_, err := repositories.UserRepository.CreateUser(user)
+	if err != nil {
+		return false
+	}
+	return true
+}
