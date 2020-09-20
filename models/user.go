e69e2860903e2ed48348c3bcb7300cbcd05b04e5
AnilYurtsever <ianilyurtsever@gmail.com>
2020-09-20 16:07:46 +0300

    user model updated

--- models/user.go
+++ models/user.go
@@ -1,7 +1,17 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type User struct {
-	Id             string `json:"_id,omitempty" bson:"_id,omitempty"`
-	Username       string `json:"username,omitempty" bson:"username,omitempty"`
-	HashedPassword string `json:"hashedPassword,omitempty" bson:"hashedPassword,omitempty"`
+	Id              primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Email           string             `json:"email,omitempty" bson:"email,omitempty"`
+	HashedPassword  string             `json:"hashedPassword,omitempty" bson:"hashedPassword,omitempty"`
+	FirstName       string             `json:"firstName,omitempty" bson:"firstName,omitempty"`
+	LastName        string             `json:"lastName,omitempty" bson:"lastName,omitempty"`
+	ThumbnailUrl    string             `json:"thumbnailUrl,omitempty" bson:"thumbnailUrl,omitempty"`
+	FriendsIdList   []string           `json:"friends,omitempty" bson:"friends,omitempty"`
+	PhotosBucketUrl string             `json:"photosBucketUrl,omitempty" bson:"photosBucketUrl,omitempty"`
+	VideosBucketUrl string             `json:"videoBucketUrl,omitempty" bson:"videosBucketUrl,omitempty"`
+	Status          bool               `json:"status,omitempty" bson:"status,omitempty"`
+	Active          bool               `json:"active,omitempty" bson:"active,omitempty"`
 }
